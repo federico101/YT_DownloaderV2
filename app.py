89378a8faa0fa618f94f143a23c0cbb484cd1ca5
fredericktulba9@gmail.com <fredericktulba9@gmail.com>
2022-05-05 17:25:17 +0800

    saving this incase some error occur

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request, url_for, redirect, send_file, session
+from pytube import YouTube
+from io import BytesIO
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "akoayisangneanderthalfromearth12!?"
+
+@app.route("/", methods = ["GET", "POST"])
+def home():
+    if request.method == "POST":
+        session['link'] = request.form.get('url')
+        try:
+            url = YouTube(session['link'])
+            url.check_availability()
+        except:
+            return render_template("error.html")
+        return render_template("dl.html", url = url)
+    return render_template("home.html")
+
+@app.route("/download", methods = ["GET", "POST"])
+def dl_video():
+    if request.method == "POST":
+        buffer = BytesIO()
+        url = YouTube(session['link'])
+        itag = request.form.get("itag")
+        video = url.streams.get_by_itag(itag)
+        video.stream_to_buffer(buffer)
+        buffer.seek(0)
+        return send_file(buffer, as_attachment=True, download_name="Video.mp4", mimetype="video/mp4")
+    return redirect(url_for("home"))
+
+if __name__ == '__main__':
+    app.run(debug=True)
